e9ad0102df850bb144a38ea6998922264b2af91a
iAutistic <68719131+iAutistic@users.noreply.github.com>
2023-03-17 11:25:55 +0500

    Add files via upload

---
+++ a3kar.py
@@ -0,0 +1,14 @@
+import requests
+import json
+from time import sleep
+
+def API():
+    res = requests.get('https://ayah.nawafdev.com/api/dekr?types=random')
+    response = json.loads(res.text)
+
+    # print(res.json())
+    print(response['content'])
+
+while True:
+    sleep(5)
+    API()
